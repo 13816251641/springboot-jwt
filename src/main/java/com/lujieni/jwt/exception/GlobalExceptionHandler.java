ef6bf8bb1d75657228f5ac64c7b375bdc371df20
admin <982584989@qq.com>
2019-10-03 20:03:44 +0800

    增加了对拦截器中异常的处理

---
+++ src/main/java/com/lujieni/jwt/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.lujieni.jwt.exception;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public JSONObject handleException(Exception e){
+        log.error("全局异常",e);
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("error",e.getMessage());
+        return jsonObject;
+    }
+}
