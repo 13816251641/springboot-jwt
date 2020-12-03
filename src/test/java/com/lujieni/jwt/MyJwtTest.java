4836ebd5119a4b1c733b7acd011c44a8014de419
181268 <lujieni1017>
2020-12-03 14:44:31 +0800

    代码提交

--- src/test/java/com/lujieni/jwt/MyJwtTest.java
+++ src/test/java/com/lujieni/jwt/MyJwtTest.java
@@ -6,13 +6,16 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.google.common.io.BaseEncoding;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Base64Utils;
 
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,17 +30,17 @@ public class MyJwtTest {
      */
     @Before
     public void createToken() {
-        Map<String, Object> map = new HashMap<String, Object>();
+        Map<String, Object> map = new HashMap();
         map.put("alg", "HS256");//HS256 对称加密算法
         map.put("typ", "JWT");
 
         /*构建密钥信息 */
         Algorithm algorithm = Algorithm.HMAC256("secret");
         token = JWT.create().withHeader(map)
+                /* 设置 载荷 Payload */
                 .withSubject("test token")//设置主题
                 .withAudience("app")//设置谁接受签名
                 .withIssuer("server")//设置签名是由谁生成
-                /* 设置 载荷 Payload */
                 .withClaim("userName", "lujieni520")
                 .withClaim("deptName", "it")
                 /* 签名 Signature */
@@ -47,7 +50,9 @@ public class MyJwtTest {
     /**
      * 直接获取token中的值,发现如下:
      * 1.audience&issuer&subject可直接获取
-     * 2.claims里的值无法直接获取,但我们可以使用Base64解密获取,所以不要把密码放在jwt里!!!
+     * 2.claims里的值需要使用asString获取
+     * 3.综上token里面存储的值默认不加密,都可以被第三方
+     * 直接读取到里面的值,签名指的是怕被第三方篡改而已!!!
      */
     @Test
     public void testGetValueDirectly(){
@@ -61,12 +66,16 @@ public class MyJwtTest {
         System.out.println("issuer:"+issuer);
         String subject = JWT.decode(token).getSubject();
         System.out.println("subject:"+subject);
+
+
         Map<String, Claim> claims = JWT.decode(token).getClaims();
-        System.out.println(claims);
+        for (Map.Entry<String, Claim> entry :claims.entrySet()){
+            System.out.println("key:"+entry.getKey()+";value:"+entry.getValue().asString());
+        }
     }
 
     /**
-     * 验证token的合法性(确认token里的参数是否被篡改了),只有通过vertify我们才能确定claim中的值是可信的
+     * 验证token的合法性(确认token里的参数是否被篡改了),只有通过vertify后我们才能确定claim中的值是可信的
      * 但audience&&issuer等参数我们却可以直接获取,见testGetValueDirectly
      */
     @Test
